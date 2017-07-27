5fd44196610e10413e31af4d2ce277d59c15ff09
mabin <mabin@youmi.net>
2017-07-27 10:39:48 +0800

    ss

--- Application/Admin/Controller/PublicController.class.php
+++ Application/Admin/Controller/PublicController.class.php
@@ -29,7 +29,10 @@ class PublicController extends \Think\Controller {
             
             /* 调用UC登录接口登录 */
             $User = new UserApi;
+            p($User);
             $uid = $User->login($username, $password);
+            p($uid);
+            die;
             if(0 < $uid){ //UC登录成功
                 /* 登录用户 */
                 $Member = D('Member');
