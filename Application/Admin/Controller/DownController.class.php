891fdcb09286e051ed395b53f762fe6a6328ea3d
mabin <mabin@youmi.net>
2017-10-10 16:19:11 +0800

    ss

--- Application/Admin/Controller/DownController.class.php
+++ Application/Admin/Controller/DownController.class.php
@@ -11,35 +11,6 @@ class DownController extends AdminController {
     }
 
     /**
-     * 用户管理首页
-     */
-    public function index(){
-        $nickname       =   I('nickname');
-        $map['status']  =   array('egt',0);
-        if(is_numeric($nickname)){
-            $map['uid|nickname']=   array(intval($nickname),array('like','%'.$nickname.'%'),'_multi'=>true);
-        }else{
-            $map['nickname']    =   array('like', '%'.(string)$nickname.'%');
-        }
-
-        $list   = $this->lists('Member', $map);
-        int_to_string($list);
-        $this->assign('_list', $list);
-        $this->meta_title = '用户信息';
-        $this->display();
-    }
-
-    /**
-     * 修改昵称初始化
-     */
-    public function reaction(){
-        $nickname = M('Member')->getFieldByUid(UID, 'nickname');
-        $this->assign('nickname', $nickname);
-        $this->meta_title = '修改昵称';
-        $this->display();
-    }
-
-    /**
      * @desc 导入订单
      * @author: mb
      * @time: 2016-09-21
@@ -199,10 +170,14 @@ class DownController extends AdminController {
      * 化合物列表
      */
     public function CompoundList(){
+    	$c_name = get_var_value( 'c_name' );
         $map['c_is_show']    =   1;
+        if(!empty($c_name)){
+        	$map['c_chemicals']    =   array("like","%{$c_name}%");
+        }
         $order = "id asc";
         $list   =   $this->lists('Compound', $map, $order);
-       // p($list);die;
+//        p($list);die;
         foreach ($list as $key=>$value){
             $list[$key]['c_add_time']    =   date("Y-m-d H:i:s",$value['c_add_time']);
             $list[$key]['c_img_url']    =   !empty($value['c_img']) ? C('IMG_HTTP_DOMAIN').$value['c_img'] : '';
@@ -217,7 +192,11 @@ class DownController extends AdminController {
      * 	反应列表
      */
     public function ReactionList(){
+    	$c_name = get_var_value( 'c_name' );
         $map['r_is_show']    =   1;
+        if(!empty($c_name)){
+        	$map['r_reactant1']    =   array("like","%{$c_name}%");
+        }
         $order = "id asc";
         $list   =   $this->lists('CompoundReaction', $map, $order);
         foreach ($list as $key=>$value){
@@ -233,171 +212,19 @@ class DownController extends AdminController {
      * 常量列表
      */
     public function ConstantList(){
+    	$c_name = get_var_value( 'c_name' );
+    	$map = array();
+        if(!empty($c_name)){
+        	$map['c_key']    =   array("like","%{$c_name}%");
+        }
         $order = "id asc";
-        $list   =   $this->lists('Constant', array(), $order);
+        $list   =   $this->lists('Constant', $map, $order);
         foreach ($list as $key=>$value){
             $list[$key]['c_add_time']    =   date("Y-m-d H:i:s",$value['c_add_time']);
         }
         $this->assign('_list', $list);
         $this->meta_title = '常量列表';
         $this->display('constantList');
-    }
-
-
-
-
-
-    /**
-     * 修改密码提交
-     */
-    public function submitPassword(){
-        //获取参数
-        $password   =   I('post.old');
-        empty($password) && $this->error('请输入原密码');
-        $data['password'] = I('post.password');
-        empty($data['password']) && $this->error('请输入新密码');
-        $repassword = I('post.repassword');
-        empty($repassword) && $this->error('请输入确认密码');
-
-        if($data['password'] !== $repassword){
-            $this->error('您输入的新密码与确认密码不一致');
-        }
-
-        $Api    =   new UserApi();
-        $res    =   $Api->updateInfo(UID, $password, $data);
-        if($res['status']){
-            $this->success('修改密码成功！');
-        }else{
-            $this->error($res['info']);
-        }
-    }
-
-    /**
-     * 用户行为列表
-     */
-    public function action(){
-        //获取列表数据
-        $Action =   M('Action')->where(array('status'=>array('gt',-1)));
-        $list   =   $this->lists($Action);
-        int_to_string($list);
-        // 记录当前列表页的cookie
-        Cookie('__forward__',$_SERVER['REQUEST_URI']);
-
-        $this->assign('_list', $list);
-        $this->meta_title = '用户行为';
-        $this->display();
-    }
-
-    /**
-     * 新增行为
-     */
-    public function addAction(){
-        $this->meta_title = '新增行为';
-        $this->assign('data',null);
-        $this->display('editaction');
-    }
-
-    /**
-     * 编辑行为
-     */
-    public function editAction(){
-        $id = I('get.id');
-        empty($id) && $this->error('参数不能为空！');
-        $data = M('Action')->field(true)->find($id);
-
-        $this->assign('data',$data);
-        $this->meta_title = '编辑行为';
-        $this->display();
-    }
-
-    /**
-     * 更新行为
-     */
-    public function saveAction(){
-        $res = D('Action')->update();
-        if(!$res){
-            $this->error(D('Action')->getError());
-        }else{
-            $this->success($res['id']?'更新成功！':'新增成功！', Cookie('__forward__'));
-        }
-    }
-
-    /**
-     * 会员状态修改
-     * @author 朱亚杰 <zhuyajie@topthink.net>
-     */
-    public function changeStatus($method=null){
-        $id = array_unique((array)I('id',0));
-        if( in_array(C('USER_ADMINISTRATOR'), $id)){
-            $this->error("不允许对超级管理员执行该操作!");
-        }
-        $id = is_array($id) ? implode(',',$id) : $id;
-        if ( empty($id) ) {
-            $this->error('请选择要操作的数据!');
-        }
-        $map['uid'] =   array('in',$id);
-        switch ( strtolower($method) ){
-            case 'forbiduser':
-                $this->forbid('Member', $map );
-                break;
-            case 'resumeuser':
-                $this->resume('Member', $map );
-                break;
-            case 'deleteuser':
-                $this->delete('Member', $map );
-                break;
-            default:
-                $this->error('参数非法');
-        }
-    }
-
-    public function add($username = '', $password = '', $repassword = '', $email = ''){
-        if(IS_POST){
-            /* 检测密码 */
-            if($password != $repassword){
-                $this->error('密码和重复密码不一致！');
-            }
-
-            /* 调用注册接口注册用户 */
-            $User   =   new UserApi;
-            $uid    =   $User->register($username, $password, $email);
-            if(0 < $uid){ //注册成功
-                $user = array('uid' => $uid, 'nickname' => $username, 'status' => 1);
-                if(!M('Member')->add($user)){
-                    $this->error('用户添加失败！');
-                } else {
-                    $this->success('用户添加成功！',U('index'));
-                }
-            } else { //注册失败，显示错误信息
-                $this->error($this->showRegError($uid));
-            }
-        } else {
-            $this->meta_title = '新增用户';
-            $this->display();
-        }
-    }
-
-    /**
-     * 获取用户注册错误信息
-     * @param  integer $code 错误编码
-     * @return string        错误信息
-     */
-    private function showRegError($code = 0){
-        switch ($code) {
-            case -1:  $error = '用户名长度必须在16个字符以内！'; break;
-            case -2:  $error = '用户名被禁止注册！'; break;
-            case -3:  $error = '用户名被占用！'; break;
-            case -4:  $error = '密码长度必须在6-30个字符之间！'; break;
-            case -5:  $error = '邮箱格式不正确！'; break;
-            case -6:  $error = '邮箱长度必须在1-32个字符之间！'; break;
-            case -7:  $error = '邮箱被禁止注册！'; break;
-            case -8:  $error = '邮箱被占用！'; break;
-            case -9:  $error = '手机格式不正确！'; break;
-            case -10: $error = '手机被禁止注册！'; break;
-            case -11: $error = '手机号被占用！'; break;
-            default:  $error = '未知错误';
-        }
-        return $error;
     }
 
 }
