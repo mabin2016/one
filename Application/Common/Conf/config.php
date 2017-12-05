25d039e5119ab72368658fd7a2ba4a02beaed2a7
mabin <mabin2005@126.com>
2017-12-05 23:08:01 +0800

    edit Application/Common/Conf/config.php

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 系统配文�?
+ * 系统配文
  * 所有系统级别的配置
  */
  
@@ -26,34 +26,34 @@ return array(
     'USER_ADMINISTRATOR' => 1, //管理员用户ID
 
     /* URL配置 */
-    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小�?true则表示不区分大小�?
+    'URL_CASE_INSENSITIVE' => true, //默认false 表示URL区分大小true则表示不区分大小
     'VAR_URL_PARAMS'       => '', // PATHINFO URL参数变量
-    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割�?
+    'URL_PATHINFO_DEPR'    => '/', //PATHINFO URL分割
 
     /* 全局过滤配置 */
     'DEFAULT_FILTER' => '', //全局过滤函数
 
-    /* 数据库配�?*/
-    'DB_TYPE'   => 'mysqli', // 数据库类�?
+    /* 数据库配*/
+    'DB_TYPE'   => 'mysqli', // 数据库类
     'DB_HOST'   => '67.218.140.159', // 服务器地址
 //     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'one', // 数据库名
-    'DB_USER'   => 'develop', // 用户�?
+    'DB_USER'   => 'develop', // 用户
     'DB_PWD'    => 'binma123...',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'one_', // 数据库表前缀
 
-    /* 文档模型配置 (文档模型核心配置，请勿更�? */
+    /* 文档模型配置 (文档模型核心配置，请勿更 */
     'DOCUMENT_MODEL_TYPE' => array(2 => '主题', 1 => '目录', 3 => '段落'),
 	
-	'SITE' => str_replace('\\','/', dirname(dirname(dirname(dirname(__FILE__))))), //网站根目�?
+	'SITE' => str_replace('\\','/', dirname(dirname(dirname(dirname(__FILE__))))), //网站根目
 	//模板缓存配置
 	'TMPL_CACHE_ON' => false,//禁止模板编译缓存
-	'HTML_CACHE_ON' => false,//禁止静态缓�?
+	'HTML_CACHE_ON' => false,//禁止静态缓
 	//网站名称
 //		'WEB_NAME'			=> 'mysite',
 	
-	 //'配置�?=>'配置�?
+	 //'配置=>'配置
 	//url配置
 	'URL_CASE_INSENSITIVE' =>true,
 	'URL_MODEL'             =>  2,//URL模式
@@ -69,8 +69,8 @@ return array(
 	'IMG_DOMAIN' => $img_domain,
 	'IMG_HTTP_DOMAIN' => 'http://'.$img_domain.'/img/',
 	'PDF_HTTP_DOMAIN' => 'http://'.$img_domain.'/pdf/',
-	'IMG_REAL_PATH' => '/var/local/my_share/uploads/one/img/',//图片地址
-	'PDF_REAL_PATH' => '/var/local/my_share/uploads/one/pdf/',//pdf地址
+	'IMG_REAL_PATH' => '/usr/share/nginx/html/one/Public/images/img/',//图片地址
+	'PDF_REAL_PATH' => '/usr/share/nginx/html/one/Public/images/pdf/',//pdf地址
 	'DOWNLOAD_DOMAIN' => $down_domain,
 	'DOWNLOAD_HTTP_DOMAIN' => 'http://'.$down_domain.'/',
 	'DOWNLOAD_REAL_PATH' => '/data/downloads/',
